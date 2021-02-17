68b4786412d162a0dd5e173f519a1e448d948b62
jai-jk <jay.jk@hotmail.com>
2021-02-17 15:14:43 +0000

    Posted params

--- app.rb
+++ app.rb
@@ -2,28 +2,32 @@ require 'sinatra'
 set :session_secret, 'secretive..'
 
 get '/' do
-  "hello!"
+    "hello!"
 end
 
 get '/secret' do
-  "shhhhhh!!"
+    "shhhhhh!!"
 end
 
 get '/batcave' do
-  "welcome to the batcave!"
+    "welcome to the batcave!"
 end
 
 get '/death' do
-  "you've reached the end!!"
+    "you've reached the end!!"
 end
 
-get '/random-cpet' do
-  @pet_names = 2.times.map { ["Amigo", "Misty", "Almond"].sample }.join(" and ")
-  erb(:index)
+get '/random-pets' do
+    @pet_names = 2.times.map { ["Amigo", "Misty", "Almond"].sample }.join(" and ")
+    erb(:index)
 end
 
-get '/named-pet' do
-  p params
-  @name = params[:name]
-  erb(:index)
+post '/named-pets' do
+    p params
+    @pet_name = params[:name]
+    erb(:index)
+end
+
+get '/name-the-pets' do
+    erb(:index) + erb(:pet_form)
 end
