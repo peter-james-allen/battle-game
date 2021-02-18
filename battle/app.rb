718871850a7aa28b37acda61a08fd6e300490fba
Peter Allen <peter_james_allen@hotmail.com>
2021-02-18 16:22:15 +0000

    added player names form

--- battle/app.rb
+++ battle/app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    "Welcome to battle"
+    erb :index
+  end
+
+  post '/named-players' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb :players
   end
 
   run! if app_file == $0
--- battle/spec/features/infrastructure_spec.rb
+++ battle/spec/features/infrastructure_spec.rb
@@ -4,3 +4,13 @@ feature 'Testing infrastructure' do
     expect(page).to have_content 'Welcome to battle'
   end
 end
+
+feature 'Test input' do
+  scenario 'enter player names' do
+    visit('/')
+    fill_in :player1, with: 'Pete'
+    fill_in :player2, with: 'Adam'
+    click_button "Submit"
+    expect(page).to have_content 'Pete vs Adam'
+  end
+end
